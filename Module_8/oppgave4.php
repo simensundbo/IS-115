341123702c209fcdd532ac7415081fc29de4c6e7
Simen Sundbø <sim123@live.no>
2021-10-26 12:06:39 +0200

    Modu 8 ferdig

--- Module_8/oppgave4.php
+++ Module_8/oppgave4.php
@@ -1,5 +1,33 @@
 <?php
 
+//sjekker om filen er deklarert
+if(isset($_GET["file"])){
+    //henter ut fil navnet
+    $file = $_GET["file"];
+    //deler navnet opp i en array
+    $arr = explode(".", $file);
+
+    //sjekker om filtypen er lik pdf
+    if(end($arr) == "pdf"){
+        //definerer mappen filen ligger i
+        $filepath = "files/".$file;
+
+        //bruker header funksjoner som forteller hva som skjer, typen og setter et navn til filen
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+
+        //leser til slutt filen som skal lastes ned
+        readfile($filepath);
+
+    }else{
+        //dersom filen ikke er en pdf
+        echo "Filtypen er ikke tillat: " . end($arr);
+    }
+}
+
 ?>
 
-<a href="files/oppgave4.pdf">Download PDF file</a>
+<p>Klikk her for å laste ned pdf-en</p>
+
+<a href="oppgave4.php?file=oppgave4.pdf">Last ned</a>
